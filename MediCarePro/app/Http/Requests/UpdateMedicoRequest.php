09d07d4232b7b975e41c6558701bfc49e5ca005a
Hugobsan <hugocorone10@gmail.com>
2024-05-15 02:58:30 -0300

    feat: edição de médico

--- MediCarePro/app/Http/Requests/UpdateMedicoRequest.php
+++ MediCarePro/app/Http/Requests/UpdateMedicoRequest.php
@@ -13,7 +13,7 @@ class UpdateMedicoRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,22 @@ class UpdateMedicoRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'nome' => 'required|string|max:255',
+            'crm' => 'required|string|max:8|min:7',
+            'especialidade' => 'required|string|max:255',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nome.required' => 'O campo nome é obrigatório',
+            'nome.max' => 'O nome deve ter no máximo 255 caracteres',
+            'crm.required' => 'O campo CRM é obrigatório',
+            'crm.max' => 'O CRM deve ter no máximo 8 caracteres',
+            'crm.min' => 'O CRM deve ter no mínimo 7 caracteres',
+            'especialidade.required' => 'O campo especialidade é obrigatório',
+            'especialidade.max' => 'A especialidade deve ter no máximo 255 caracteres',
         ];
     }
 }
